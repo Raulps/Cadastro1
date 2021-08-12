d92f48f57fc23a76299ae77f2950d298f0630910
Raulps <74885838+Raulps@users.noreply.github.com>
2021-08-12 16:55:08 -0300

    Update relatorio.php

--- relatorio.php
+++ relatorio.php
@@ -4,7 +4,8 @@ $sql="SELECT * FROM cadastro";
 $resultado=mysqli_query($conexao,$sql);
 $achou=mysqli_num_rows($resultado);
 
-echo"<meta charset='utf-8'><table width='60%' border='1' bordercolor='gray'> 
+echo"<meta charset='utf-8'><table width='60%' border='1' bordercolor='gray'>
+<link rel='stylesheet' type='text/css' href='stylerelatorio.css'>
 <tr><td colspan='9' align='center'><h2>Relatório</h2></td></tr>
 <tr><td>ID</td><td>Nome</td><td>Sobrenome</td><td>Gênero</td><td>Telefone</td><td>CEP</td><td>E-mail</td><td colspan='2'>Ação</td>";
 
@@ -19,7 +20,19 @@ echo"<tr><td>$res[0]</td>
 <td>$res[4]</td>
 <td>$res[5]</td>
 <td>$res[6]</td>
-<td><a class="editar" href='editar2.php?id=$id'>Editar</td><td><a class="excluir" href='excluir.php?id=$id'>Excluir</td></tr>";
+<td><a class='editar' href='editar2.php?id=$id'>Editar</td><td><a class='excluir' href='excluir.php?id=$id'>Excluir</td></tr>";
 }
 echo"</table>"
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>M+M Relatório</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
